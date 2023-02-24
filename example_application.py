0704b422317e4800e9da7ea5d80d701ff4ae6787
plkn <fischmechanik@gmail.com>
2023-02-24 14:25:17 +0100

    loops in example added

--- example_application.py
+++ example_application.py
@@ -8,7 +8,7 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
 # Import cfa removal module
-path_file = "path_to_module" # Insert path to "remove_cfa" folder.
+path_file = "path_to_module"  # Insert path to "remove_cfa" folder.
 sys.path.insert(1, path_file)
 from remove_cfa import remove_cfa
 
@@ -17,41 +17,60 @@ path_in = "path_to_preprocessed data"
 
 
 # Example subject and channel
-subj = "VP02" # Subject identifier
-chan = 40 # Channel number as integer
+subjects = ["VP02"]  # List of subject identifiers
+channels = [40]  # List of channels as integer
 
-# Load training data
-X_data_rlocked = np.asarray(
-    loadmat(os.path.join(path_in, f"{subj}_train_X"))["train_X"]
-).astype("float64")
-y_data_rlocked = np.asarray(
-    loadmat(os.path.join(path_in, f"{subj}_chan{str(chan)}_train_y",))["train_y"]
-).astype("float64")
+# loop subjects
+for subj in subjects:
 
-# Load stimlocked data
-X_data_stimlocked = np.asarray(
-    loadmat(os.path.join(path_in, f"{subj}_predi_X"))["predi_X"]
-).astype("float64")
-y_data_stimlocked = np.asarray(
-    loadmat(os.path.join(path_in, f"{subj}_chan{str(chan)}_predi_y",))["predi_y"]
-).astype("float64")
+    # Loop channels
+    for chan in channels:
 
+        # Load training data
+        X_data_rlocked = np.asarray(
+            loadmat(os.path.join(path_in, f"{subj}_train_X"))["train_X"]
+        ).astype("float64")
+        y_data_rlocked = np.asarray(
+            loadmat(
+                os.path.join(
+                    path_in,
+                    f"{subj}_chan{str(chan)}_train_y",
+                )
+            )["train_y"]
+        ).astype("float64")
 
-# Remove last 5 trials due to missing values in cardiac phase info
-X_data_rlocked = X_data_rlocked[:, :, :-5].copy()
-y_data_rlocked = y_data_rlocked[:, :-5].copy()
+        # Load stimlocked data
+        X_data_stimlocked = np.asarray(
+            loadmat(os.path.join(path_in, f"{subj}_predi_X"))["predi_X"]
+        ).astype("float64")
+        y_data_stimlocked = np.asarray(
+            loadmat(
+                os.path.join(
+                    path_in,
+                    f"{subj}_chan{str(chan)}_predi_y",
+                )
+            )["predi_y"]
+        ).astype("float64")
 
-# Feature data is features x times x trials
-# EEG data is times x trials
+        # Remove last 5 trials due to missing values in cardiac phase info
+        X_data_rlocked = X_data_rlocked[:, :, :-5].copy()
+        y_data_rlocked = y_data_rlocked[:, :-5].copy()
 
-# Call function
-y_data_stimlocked_2d, y_stimlocked_predicted_2d, y_stimlocked_clean_2d = remove_cfa(
-    X_data_rlocked,
-    y_data_rlocked,
-    X_data_stimlocked,
-    y_data_stimlocked,
-    n_obs_train=1000000,
-)
+        # Feature data is features x times x trials
+        # EEG data is times x trials
+
+        # Call function
+        (
+            y_data_stimlocked_2d,
+            y_stimlocked_predicted_2d,
+            y_stimlocked_clean_2d,
+        ) = remove_cfa(
+            X_data_rlocked,
+            y_data_rlocked,
+            X_data_stimlocked,
+            y_data_stimlocked,
+            n_obs_train=1000000,
+        )
 
 
 # Plot
